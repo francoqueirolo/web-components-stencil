6f0a95f9a39bb3bb0111e71116856003c3a5c8d1
francoqueirolo <fgqueirolo@gmail.com>
2020-03-05 11:45:37 -0500

    feat: using state

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -1,4 +1,4 @@
-import { h, Component, Prop } from '@stencil/core';
+import { h, Component, Prop, State } from '@stencil/core';
 
 @Component({
   tag: 'uc-side-drawer',
@@ -6,6 +6,8 @@ import { h, Component, Prop } from '@stencil/core';
   shadow: true
 })
 export class SideDrawer {
+  @State() showContactInfo = false;
+
   @Prop({ reflect: true }) title: string;
   @Prop({ reflect: true, mutable: true }) open: boolean;
 
@@ -13,16 +15,51 @@ export class SideDrawer {
     this.open = false;
   }
 
+  onContentChange(content: string) {
+    this.showContactInfo = content === 'contact';
+  }
+
   render() {
+    let mainContent = <slot />;
+    if (this.showContactInfo) {
+      mainContent = (
+        <div id="contact-information">
+          <h2>Contact Information</h2>
+          <p>You can reach us via phone or email.</p>
+          <ul>
+            <li>Phone: 49802384032</li>
+            <li>
+              E-Mail:
+              <a href="mailto:something@something.com">
+                something@something.com
+              </a>
+            </li>
+          </ul>
+        </div>
+      );
+    }
+
     return (
       <aside>
         <header>
           <h1>{this.title}</h1>
           <button onClick={this.onCloseDrawer.bind(this)}>X</button>
         </header>
-        <main>
-          <slot />
-        </main>
+        <section id="tabs">
+          <button
+            class={!this.showContactInfo ? 'active' : ''}
+            onClick={this.onContentChange.bind(this, 'nav')}
+          >
+            Navigation
+          </button>
+          <button
+            class={this.showContactInfo ? 'active' : ''}
+            onClick={this.onContentChange.bind(this, 'contact')}
+          >
+            Contact
+          </button>
+        </section>
+        <main>{mainContent}</main>
       </aside>
     );
   }
